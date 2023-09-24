7ca039b9bc9547232f262994078c3e3fa3df4c15
Chinmay01234 <chinmayjavalagi@gmail.com>
2023-09-24 09:13:15 +0530

    pallete.dart file added

---
+++ lib/utils/pallete.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color primaryThemeColor = Color(0x0015b082);
+  static const Color grayColor = Color(0x00909090);
+  static const Color lightGrayColor = Color(0x00eeeeee);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
